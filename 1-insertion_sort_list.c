c53c53b956d851db7515ab1ee6130574ff6633cb
Nada <nadafarrag619@gmail.com>
2024-01-21 21:32:50 +0200

    file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,32 +1,33 @@
 #include "sort.h"
 /**
-* insertion_sort_list - insertion sorting algorithm
-* @list: linked list to sort
+*insertion_sort_list-insertion sorting
+*@list:linked list 
 */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *t;
-int y;
-if (!list)
-return;
-t = *list;
-while (t)
-{
-while (t)
-{
-if (t->next)
-{
-if (tmp->y > tmp->next->y)
-{
-y = t->y;
-*(int *)&t->y = t->next->y;
-*(int *)&t->next->y = y;
-t = *list;
-print_list(*list);
-break;
-}
-}
-t = t->next;
-}
-}
+	listint_t *tmp;
+	int y;
+
+	if (!list)
+		return;
+	tmp = *list;
+	while (tmp)
+	{
+		while (tmp)
+		{
+			if (tmp->next)
+			{
+				if (tmp->y > tmp->next->y)
+				{
+					y = tmp->y;
+					*(int *)&tmp->y = tmp->next->y;
+					*(int *)&tmp->next->y = y;
+					tmp = *list;
+					print_list(*list);
+					break;
+				}
+			}
+			tmp = tmp->next;
+		}
+	}
 }
