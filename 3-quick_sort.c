edd012cedda9b1c894721519a9c848dfe2a3c57c
Nada <nadafarrag619@gmail.com>
2024-01-21 22:08:17 +0200

    file

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,31 +1,5 @@
 #include "sort.h"
 /**
-*quick_sort-sorts an array of integers
-*@array:array to sort
-*@size:array size
-*/
-void quick_sort(int *array, size_t size)
-{
-	quiz(array, 0, size - 1, size);
-}
-/**
-*quiz-sorts an array of integers
-*@array:array to sort
-*@first:position
-*@last:position
-*@size: array size
-*/
-void quiz(int *array, int first, int last, size_t size)
-{
-	int p;
-	if (first < last)
-	{
-		p = partition(array, first, last, size);
-		quiz(array, first, p - 1, size);
-		quiz(array, p + 1, last, size);
-	}
-}
-/**
 *partition-array partition
 *@array:array to sort
 *@first:position
@@ -59,4 +33,31 @@ int partition(int *array, int first, int last, size_t size)
 	}
 
 	return (i + 1);
+}
+/**
+*quiz-sort array of integers
+*@array:array to sort
+*@first:first position
+*@last:last position
+*@size:array size
+*/
+void quiz(int *array, int first, int last, size_t size)
+{
+	int ;
+	if (first < last)
+	{
+		p = partition(array, first, last, size);
+		quiz(array, first, p - 1, size);
+		quiz(array, p + 1, last, size);
+	}
+}
+
+/**
+*quick_sort-sorts an array of integers
+*@array: array to sort
+*@size: array size
+*/
+void quick_sort(int *array, size_t size)
+{
+	quiz(array, 0, size - 1, size);
 }
